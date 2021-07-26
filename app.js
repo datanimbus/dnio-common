eb6717052ac4e017fd92a2db8d5c9a7d82ee1b99
Jugnu Agrawal <jugnu@appveen.com>
2021-07-26 15:45:09 +0530

    SIGTERM Handler

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const preProcessor = require('./utils/pre-processor.utils');
 const LOGGER_NAME = config.isK8sEnv() ? `[${config.hostname}] [COMMON v${config.imageTag}]` : `[COMMON v${config.imageTag}]`
 const logger = log4js.getLogger(LOGGER_NAME);
 logger.level = process.env.LOG_LEVEL || 'info';
+global.activeRequest = 0;
 
 // global.logger = logger;
 
@@ -24,7 +25,18 @@ app.use(express.json({ inflate: true }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-    logger.info(req.method, req.path, req.query);
+    if (req.path.split('/').indexOf('live') == -1 && req.path.split('/').indexOf('ready') == -1) {
+        logger.info(req.method, req.path, req.query);
+        logger.trace(`[${req.get(global.txnIdHeader)}] req.path : ${req.path}`);
+        logger.trace(`[${req.get(global.txnIdHeader)}] req.headers : ${JSON.stringify(req.headers)} `);
+    }
+    global.activeRequest++;
+    res.on('close', function () {
+        global.activeRequest--;
+        if (req.path.split('/').indexOf('live') == -1 && req.path.split('/').indexOf('ready') == -1) {
+            logger.debug(`[${req.get(global.txnIdHeader)}] Request completed for ${req.originalUrl}`);
+        }
+    });
     next();
 });
 
@@ -41,4 +53,31 @@ https.createServer({
     key: fs.readFileSync(path.join(process.cwd(), 'keys', 'txn.key'))
 }, app).listen(config.httpsPort, () => {
     logger.info('HTTPs Server Listening on PORT:', config.httpsPort);
+});
+
+
+process.on('SIGTERM', () => {
+    try {
+        // Handle Request for 15 sec then stop recieving
+        setTimeout(() => {
+            global.stopServer = true;
+        }, 15000);
+        logger.info('Process Kill Request Recieved');
+        const intVal = setInterval(() => {
+            // Waiting For all pending requests to finish;
+            if (global.activeRequest === 0) {
+                // Closing Express Server;
+                server.close(() => {
+                    logger.info('Server Stopped.');
+                    process.exit(0);
+                });
+                clearInterval(intVal);
+            } else {
+                logger.info('Waiting for request to complete, Active Requests:', global.activeRequest);
+            }
+        }, 2000);
+    } catch (e) {
+        logger.error('SIGTERM Handler', e);
+        process.exit(0);
+    }
 });
